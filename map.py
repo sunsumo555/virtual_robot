41500b373e0a00bb2789e524d5d96e9cc55eb452
Sirapoab Chaikunsaeng <sc6918@texel34.doc.ic.ac.uk>
2018-12-02 20:04:43 +0000

    updated map and state

--- map.py
+++ map.py
@@ -10,90 +10,90 @@ class Map:
         self.r = robot.Robot(starting_x = starting_x,
                              starting_y = starting_y,
                              n_particles = 20,
-                             motion_sigma_distance=0.01, 
-                             motion_sigma_angle=0.001, 
+                             motion_sigma_distance=0.01,
+                             motion_sigma_angle=0.001,
                              rotation_sigma_angle=0.001)
-        
+
         self.walls = mc.walls
         self.enlargement_factor = enlargement_factor
         self.x_padding = x_padding
         self.y_padding = y_padding
-        
+
         self.print_weights = True
         self.sleep_time = 2
-        
+
         #for debugging only, dont open it!
         self.r.particle_cloud.randomize_particles(70,90,20,40)
-        
+
         self.measurement_var = 50
-               
+
         self.draw_particles()
         self.pi = 3.14159
-        
+
     def report_robot(self):
         print(">>> robot at x="+str(self.r.particle_cloud.avg_x) +" y=" +str(self.r.particle_cloud.avg_y)+" theta="+str(self.r.particle_cloud.avg_theta * 180 / self.pi))
-        
+
     def draw_map(self):
         for wall in self.walls:
-            line = (self.enlargement_factor*wall[0]+self.x_padding, 
-                    self.enlargement_factor*wall[1]+self.y_padding, 
-                    self.enlargement_factor*wall[2]+self.x_padding, 
+            line = (self.enlargement_factor*wall[0]+self.x_padding,
+                    self.enlargement_factor*wall[1]+self.y_padding,
+                    self.enlargement_factor*wall[2]+self.x_padding,
                     self.enlargement_factor*wall[3]+self.y_padding)
             print "drawLine:" + str(line)
-            
+
     def draw_particles(self):
         if self.print_weights:
-            print_vector = [(self.enlargement_factor*p.x + self.x_padding, 
-                             self.enlargement_factor*p.y + self.y_padding, 
+            print_vector = [(self.enlargement_factor*p.x + self.x_padding,
+                             self.enlargement_factor*p.y + self.y_padding,
                              self.enlargement_factor*p.theta,w*100) for p,w in zip(self.r.particle_cloud.particles,self.r.particle_cloud.weights)]
             print "drawParticles:" + str(print_vector)
-            
+
         else:
             print_vector = [(p.x + self.x_padding, p.y + self.y_padding, p.theta) for p in self.r.particle_cloud.particles]
             print "drawParticles:" + str(print_vector)
-        
+
         crosshair_y = (self.enlargement_factor*self.r.particle_cloud.avg_x,
                        self.enlargement_factor*self.r.particle_cloud.avg_y-self.enlargement_factor*3,
                        self.enlargement_factor*self.r.particle_cloud.avg_x,
                        self.enlargement_factor*self.r.particle_cloud.avg_y+self.enlargement_factor*3)
         print "drawLine:" + str(crosshair_y)
-        
+
         crosshair_x = (self.enlargement_factor*self.r.particle_cloud.avg_x-self.enlargement_factor*3,
                        self.enlargement_factor*self.r.particle_cloud.avg_y,
                        self.enlargement_factor*self.r.particle_cloud.avg_x+self.enlargement_factor*3,
-                       self.enlargement_factor*self.r.particle_cloud.avg_y)        
+                       self.enlargement_factor*self.r.particle_cloud.avg_y)
         print "drawLine:" + str(crosshair_x)
-        
+
     def move_robot_forward(self,d,use_slam = False):
         if use_slam:
             initial_x = self.r.particle_cloud.avg_x
             initial_y = self.r.particle_cloud.avg_y
             initial_theta = self.r.particle_cloud.avg_theta
-            
+
             target_x = initial_x + d*math.cos(initial_theta)
             target_y = initial_y + d*math.sin(initial_theta)
-            
+
             for i in range(10):
                 self.r.travel_forwards(d/10.0)
                 self.draw_particles()
-                self.update_particle_all()   
+                self.update_particle_all()
                 self.draw_particles()
                 time.sleep(0.1)
 
             current_x = self.r.particle_cloud.avg_x
             current_y = self.r.particle_cloud.avg_y
             current_theta = self.r.particle_cloud.avg_theta
-            
+
             distance_error = math.sqrt((current_x-target_x)**2 + (current_y-target_y)**2)
             self.draw_particles()
-            
+
             if abs(distance_error) > 1.0:
                 self.move_robot_forward(distance_error,use_slam=False)
-        
+
         else:
             self.r.travel_forwards(d)
             self.draw_particles()
-        
+
     def rotate_robot(self,radian,direction = 'ccw',use_slam = False):
         if direction == 'ccw':
             self.r.rotate_on_spot(radian)
@@ -103,7 +103,7 @@ class Map:
             self.draw_particles()
         else:
             print "invalid direction command on Map.rotate_robot: please input ccw or cw"
-        
+
     def move_robot_to(self,target_x,target_y):
         """
         Navigates to the given waypoint and stops.
@@ -114,17 +114,17 @@ class Map:
         x_diff = target_x - self.r.particle_cloud.avg_x
         y_diff = target_y - self.r.particle_cloud.avg_y
         tol = 10**-3
-        
+
         # if the distance is not that great, forget it
         if abs(x_diff) < tol and abs(y_diff) < tol:
             return None
 
         world_frame_angle = math.atan2(y_diff,x_diff)
         robot_frame_angle = world_frame_angle - self.r.particle_cloud.avg_theta
-        
+
         if robot_frame_angle < -self.pi:
             robot_frame_angle += 2*self.pi
-            
+
         if robot_frame_angle > self.pi:
             robot_frame_angle -= 2*self.pi
 
@@ -142,23 +142,23 @@ class Map:
         #self.report_robot()
 
         time.sleep(2)
-        
+
     def move_robot_to_slam(self,target_x,target_y):
         print("moving first half")
         x,y,theta = self.r.where_am_i()
         self.move_robot_to((target_x + x)/2.0,(target_y + y)/2.0)
         time.sleep(2)
-        
+
         print("localizing robot")
         self.scan_sonar_and_update_batch()
         time.sleep(2)
-        
+
         print("moving second half")
         self.move_robot_to(target_x,target_y)
         time.sleep(2)
-        
+
         self.scan_sonar_and_update_batch()
-                
+
     def move_robot_to2(self,target_x,target_y, use_slam = False):
         """
         Navigates to the given waypoint and stops.
@@ -169,17 +169,17 @@ class Map:
         x_diff = target_x - self.r.particle_cloud.avg_x
         y_diff = target_y - self.r.particle_cloud.avg_y
         tol = 10**-3
-        
+
         # if the distance is not that great, forget it
         if abs(x_diff) < tol and abs(y_diff) < tol:
             return None
 
         world_frame_angle = math.atan2(y_diff,x_diff)
         robot_frame_angle = world_frame_angle - self.r.particle_cloud.avg_theta
-        
+
         if robot_frame_angle < -self.pi:
             robot_frame_angle += 2*self.pi
-            
+
         if robot_frame_angle > self.pi:
             robot_frame_angle -= 2*self.pi
 
@@ -196,36 +196,36 @@ class Map:
             n_steps = 5.0
             for i in range(int(n_steps)):
                 self.move_robot_forward(travel_distance/n_steps,use_slam = False)
-                self.update_particle_all() 
+                self.update_particle_all()
                 self.draw_particles()
                 time.sleep(1)
-                
+
                 current_x = self.r.particle_cloud.avg_x
                 current_y = self.r.particle_cloud.avg_y
                 current_theta = self.r.particle_cloud.avg_theta
                 print(">>> World_frame_angle = "+str(world_frame_angle)+" current_angle = "+str(current_theta) + " error = " + str(world_frame_angle - current_theta))
-                
+
                 if abs(world_frame_angle - current_theta) > 0.016:
                     print(">>> Robot out of course, world_frame_angle = "+str(world_frame_angle)+" current_angle = "+str(current_theta))
                     #self.move_robot_to(target_x,target_y, use_slam = False)
                     self.rotate_robot(world_frame_angle - current_theta,direction='ccw',use_slam=False)
                     time.sleep(1)
-                                        
+
             print("Moved forwards " + str(travel_distance) + " meters")
-            
-            self.update_particle_all() 
+
+            self.update_particle_all()
             self.draw_particles()
             time.sleep(1)
-                
+
             current_x = self.r.particle_cloud.avg_x
             current_y = self.r.particle_cloud.avg_y
             current_theta = self.r.particle_cloud.avg_theta
-            
+
             distance_error = math.sqrt((target_x-current_x)**2 + (target_y - current_y)**2)
             print("Performing final correction of " + str(distance_error) + " meters")
             self.move_robot_forward(distance_error,use_slam=False)
             print("Destination reached")
-            
+
         else:
             print("Moving forwards " + str(travel_distance) + " meters")
             self.move_robot_forward(travel_distance,use_slam=False)
@@ -233,51 +233,51 @@ class Map:
             #self.report_robot()
 
         time.sleep(self.sleep_time)
-        
+
     def get_robot_measurement(self):
         return self.r.sonar_sensor.value()[0]
-    
+
     def update_particle_weights(self):
         measurement_distance = self.get_robot_measurement()
         self.r.particle_cloud.update_weights(measurement_distance, self.measurement_var)
         self.draw_particles()
-    
+
     def update_particle_all(self):
         measurement_distance = self.get_robot_measurement()
-        self.r.particle_cloud.update_cloud_from_measurement(measurement_distance,self.measurement_var)        
+        self.r.particle_cloud.update_cloud_from_measurement(measurement_distance,self.measurement_var)
         self.draw_particles()
-        
+
     def update_particles_at_angle(self,theta):
         self.r.rotate_sonar_to(theta)
         time.sleep(1)
         self.update_particle_all()
-    
+
     def scan_sonar_and_update(self):
         for theta in np.linspace(-1*math.pi,math.pi,7):
             # rotate the sonar by 30 degree from -180 to 180 (7 steps)
             self.update_particles_at_angle(theta)
-            
+
         self.r.rotate_sonar_to(0.0)
-        
+
     def scan_sonar_and_update_batch(self):
         thetas = []
         measurements = []
-        
+
         print(">>> taking measurements")
-        
+
         self.r.rotate_sonar_to(-1*math.pi)
         time.sleep(2)
-        
+
         for theta in np.linspace(-1*math.pi,5.0/6.0*math.pi,10):
             # rotate the sonar by 30 degree from -180 to 120 (6 steps)
             thetas.append(theta)
-            
+
             self.r.rotate_sonar_to(theta)
             time.sleep(1.5)
             measurements.append(self.get_robot_measurement())
-                        
+
         print(measurements)
-        
+
         self.r.rotate_sonar_to(0.0)
         print(">>> updating particles")
         #the first measurement is often errorneous
--- state.py
+++ state.py
@@ -35,19 +35,19 @@ class WorldPosition:
 
     def rotate(self, theta):
         new_theta = self.theta + theta + error(self.rotation_sigma_angle)*theta
-        
+
         self.theta = new_theta
-        
+
         #self.renormalize_theta()
-        
+
     def renormalize_theta(self):
         while self.theta > pi:
             self.theta -= 2*pi
-            
+
         while self.theta <= -1*pi:
             self.theta += 2*pi
-    
-        
+
+
 class ParticleCloud:
 
     def __init__(self, n_particles=100, x=0, y=0, theta=0,
@@ -65,16 +65,16 @@ class ParticleCloud:
         self.weights = 1.0/self.n_particles * np.ones(self.n_particles)
 
         self.update_average_position()
-        
+
         #sonar angle will be between -180 to 180 degrees
         self.sonar_angle = 0
-        
-    def randomize_particles(self,x_lower_bound,x_upper_bound,y_lower_bound,y_upper_bound, x_0=10, y_0=10, t_0=0):
+
+    def randomize_particles(self,x_lower_bound,x_upper_bound,y_lower_bound,y_upper_bound):
         i = 0
-        self.particles[0].x = x_0
-        self.particles[0].y = y_0
-        self.particles[0].theta = t_0
-        
+        self.particles[0].x = 84
+        self.particles[0].y = 30
+        self.particles[0].theta = 0
+
         for p in self.particles[1:]:
             i+=1
             x = (x_upper_bound - x_lower_bound) * np.random.random_sample() + x_lower_bound
@@ -82,11 +82,11 @@ class ParticleCloud:
             #x = i/self.n_particles * upper_bound
             theta = 2*3.14135 * np.random.random_sample() - 3.14135
             theta = 0
-            
+
             p.x = x
             p.y = y
             p.theta = theta
-    
+
     def reset_weight_matrix(self):
         self.weights = 1.0/self.n_particles * np.ones(self.n_particles)
 
@@ -97,11 +97,11 @@ class ParticleCloud:
         #              particle.y+padding_y,
         #              particle.theta,
         #              weight*100) for particle,weight in zip(self.particles,self.weights.flatten())]
-        
+
         particles = [(particle.x+padding_x,
                       particle.y+padding_y,
                       particle.theta) for particle in self.particles]
-        
+
         #return "drawParticles:" + str(particles)
         return particles
 
@@ -127,15 +127,15 @@ class ParticleCloud:
         new_weights = []
         for index, particle in enumerate(self.particles):
             new_weight = mc.calculate_likelihood(
-                            [particle.x, particle.y, particle.theta + self.sonar_angle], 
-                            distance_observed, 
+                            [particle.x, particle.y, particle.theta + self.sonar_angle],
+                            distance_observed,
                             measurement_sigma)
-            
+
             new_weights.append(new_weight)
 
         weights_sum = np.sum(new_weights)
         self.weights = np.array(new_weights)/weights_sum
-        
+
     def update_weights_batch(self, thetas, measurements, measurement_sigma):
         new_weights = []
         #print(thetas)
@@ -154,33 +154,28 @@ class ParticleCloud:
                     calc_list.append(255)
                     continue
 #                new_weight *= mc.calculate_likelihood(
-#                                [particle.x, particle.y, particle.theta + theta], 
-#                                measurement, 
+#                                [particle.x, particle.y, particle.theta + theta],
+#                                measurement,
 #                                measurement_sigma)
                 wall, min_distance_to_wall = mc.find_relevant_wall((particle.x,particle.y,particle.theta + theta), mc.walls)
                 if wall == "particle out of bounds":
                     min_distance_to_wall = 9999
-                
+
                 calc_list.append(min_distance_to_wall)
-            
+
             print("index = "+str(index) + "x = "+str(particle.x) + "y = "+ str(particle.y) + " z = "+ str(particle.theta))
             print(calc_list)
             print(measurements)
             new_weight = mc.calculate_likelihood_multivariate(calc_list, measurements, measurement_sigma)
             new_weights.append(new_weight)
-        
+
         print("new weights:")
         for i,w in enumerate(new_weights):
             print("i = "+str(i)+" w = "+str(w))
-        
         weights_sum = np.sum(new_weights)
         self.weights = np.array(new_weights)/weights_sum
 
-        print("final weights after normalized by",weights_sum)
-        for i,w in enumerate(self.weights):
-            print("i = "+str(i)+" w = "+str(w))
-
-    def update_cloud_from_measurement(self, distance_observed, 
+    def update_cloud_from_measurement(self, distance_observed,
                                             measurement_var=3):
         """
         Given the measurement of the sonar, update the particle cloud.
@@ -192,30 +187,25 @@ class ParticleCloud:
         self.resample()
         self.update_average_position()
         self.reset_weight_matrix()
-        
-    def update_cloud_from_measurement_batch(self, thetas, measurements, measurement_var=3, weights_only = False):
+
+    def update_cloud_from_measurement_batch(self, thetas, measurements, measurement_var=3):
         '''update the particle cloud using a list of measurements'''
         self.update_weights_batch(thetas, measurements, measurement_var)
-        if weights_only:
-            return
         self.resample()
         self.update_average_position()
-        self.reset_weight_matrix()        
+        self.reset_weight_matrix()
 
-    def resample_np(self):
-        self.particles = np.random.choice(self.particles, self.n_particles ,p=self.weights)
-        
     def renormalize_avg_theta(self):
         while self.avg_theta > pi:
             self.avg_theta -= 2*pi
-            
+
         while self.avg_theta <= -1*pi:
             self.avg_theta += 2*pi
-            
+
     def renormalize_thetas(self):
         for p in self.particles:
             p.renormalize_theta()
-    
+
     def update_average_position(self):
         """
         Update the average postion of the particle cloud
@@ -228,15 +218,15 @@ class ParticleCloud:
         self.avg_y = self.weights.dot(y_vector)
         self.avg_theta = self.weights.dot(theta_vector)
         self.renormalize_avg_theta()
-        
+
         if abs(self.avg_theta - pi) > pi/6.0:
             self.renormalize_thetas()
-            
+
     def resample(self):
         """
         Given the weight matrix, create a new particle set.
         """
-        
+
         #print("RESAMPLING")
         #print("STATE: particles before")
         #self.print_all_cloud()
@@ -249,7 +239,6 @@ class ParticleCloud:
             running_total += self.weights[weight]
             acc_weights.append(running_total)
         acc_weights[-1] = 1.0    # Enforces the list of weights to sum to 1. This avoids rounding errors.
-
         for i in range(len(self.weights)):
             rand_particle = np.random.random()
             if rand_particle < min(self.weights):
@@ -257,15 +246,14 @@ class ParticleCloud:
             for particle in range(len(self.particles)):
                 if rand_particle < acc_weights[particle]:
                     break
-                    
+
             #create new particle instead of copying
-            print("resampling: picked particle %s"%(particle-1))
             new_particle = WorldPosition(x = self.particles[particle - 1].x,
                                          y = self.particles[particle - 1].y,
                                          theta = self.particles[particle - 1].theta,
                                          motion_sigma_distance = self.particles[particle - 1].motion_sigma_distance,
                                          motion_sigma_angle = self.particles[particle - 1].motion_sigma_angle,
-                                         rotation_sigma_angle = self.particles[particle - 1].rotation_sigma_angle) 
+                                         rotation_sigma_angle = self.particles[particle - 1].rotation_sigma_angle)
 
             #new_particles.append(self.particles[particle - 1])
             new_particles.append(new_particle)
@@ -281,7 +269,7 @@ class ParticleCloud:
         data = np.vstack([x_vector, y_vector, theta_vector]).T
 
         return np.cov(data, bias=True)
-    
+
     def print_all_cloud(self):
         i=0
         for particle, weight in zip(self.particles, self.weights):
@@ -290,14 +278,13 @@ class ParticleCloud:
             #if(i%5) != 0:
             #    continue
             print("x = "+str(particle.x)+" y = "+str(particle.y)+" t = "+str(particle.theta)+" w = "+str(weight))
-            
+
     def rotate_sonar(self,theta):
         #ccw is positive, cw is negative angle
         self.sonar_angle += theta
-            
+
 if __name__ in "__main__":
     p = ParticleCloud()
     w = WorldPosition()
-    
-    p.resample()
 
+    p.resample()
